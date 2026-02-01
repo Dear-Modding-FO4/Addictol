7fdc7ddccc6bb4a9fd38d01e2a3077db8af9ce6b
Alexey <vlad.shabalin.1992@mail.ru>
2026-02-01 14:04:44 +0300

    Fix CTD OG game init

--- Addictol/Source/AdIDs.cpp
+++ Addictol/Source/AdIDs.cpp
@@ -6,6 +6,8 @@ namespace RE_MERGE::ID
 {
 	static void UpdateIDs_OG() noexcept
 	{
+		MessageBoxA(0, "", "", 0);
+
 		RELEX::UpdateID(RE::ID::Setting::INISettingCollection::Singleton, 791183);
 		RELEX::UpdateID(RE::ID::Setting::INIPrefSettingCollection::Singleton, 767844);
 	}
--- Addictol/Source/AdPlugin.cpp
+++ Addictol/Source/AdPlugin.cpp
@@ -1,11 +1,19 @@
 #include <AdPlugin.h>
 #include <AdIDs.h>
 #include <REX\REX\Singleton.h>
+#if AD_DEBUGBREAK
+#	include <windows.h>
+#endif
 
 extern void AdRegisterModules();
 
 namespace Addictol
 {
+#if SUPPORT_OG
+	// For support OG
+	static F4SE::Impl::F4SEInterface RestoreLoadInterface;
+#endif // SUPPORT_OG
+
 	static void F4SEMessageListener(F4SE::MessagingInterface::Message* a_msg) noexcept
 	{
 		auto plugin = Plugin::GetSingleton();
@@ -24,36 +32,61 @@ namespace Addictol
 		}
 	}
 
+#if SUPPORT_OG
+	// Added F4SE 0.7.1+
+	static  const char* F4SEAPI F4SEGetSaveFolderName() noexcept
+	{
+		return "Fallout4";
+	}
+#endif // SUPPORT_OG
+
 	bool Plugin::Init(const F4SE::LoadInterface* a_f4se)
 	{
 		if (isInit)
 			return true;
 
-		// Init
-		F4SE::Init(a_f4se);
-		REX::INFO("" _PluginName " mod (ver: " VER_FILE_VERSION_STR ") Initializing...");
+		static std::once_flag once;
+		std::call_once(once, [&]() {
+#if AD_DEBUGBREAK
+			MessageBoxA(0, "Debugbreak", "DEBUG", 0);
+#endif
+#if SUPPORT_OG
+			memcpy(&RestoreLoadInterface, a_f4se, 48 /* OG struct size */);
+			(((F4SE::Impl::F4SEInterface*)(&RestoreLoadInterface))->GetSaveFolderName) = F4SEGetSaveFolderName;
+			// Init
+			F4SE::Init((const F4SE::LoadInterface*)(&RestoreLoadInterface));
+#else
+			// Init
+			F4SE::Init(a_f4se);
+#endif // SUPPORT_OG
 
-		// Update IDs for commonlibf4
-		RE_MERGE::ID::UpdateIDs();
+			auto game_ver = a_f4se->RuntimeVersion();
+			REX::INFO("" _PluginName " mod (ver: " VER_FILE_VERSION_STR ") Initializing...");
+			REX::INFO("Game version: {}.{}.{}.{}", game_ver.major(), game_ver.minor(), game_ver.patch(), game_ver.build());
 
-		// Register all modules
-		AdRegisterModules();
+			// Update IDs for commonlibf4
+			RE_MERGE::ID::UpdateIDs();
 
-		// Load the Config
-		const auto config = REX::TOML::SettingStore::GetSingleton();
-		config->Init("Data/F4SE/Plugins/" _PluginName ".toml", "Data/F4SE/Plugins/" _PluginName "Custom.toml");
-		config->Load();
+			// Register all modules
+			AdRegisterModules();
 
-		// Listen for Messages (to Install PostInit Patches)
-		auto MessagingInterface = F4SE::GetMessagingInterface();
-		MessagingInterface->RegisterListener(F4SEMessageListener);
-		REX::INFO("Started Listening for F4SE Message Callbacks.");
+			// Load the Config
+			const auto config = REX::TOML::SettingStore::GetSingleton();
+			config->Init("Data/F4SE/Plugins/" _PluginName ".toml", "Data/F4SE/Plugins/" _PluginName "Custom.toml");
+			config->Load();
 
-		// Install preload patches
-		moduleManager.QueryPreloadAll();
-		moduleManager.InstallPreloadAll();
+			// Listen for Messages (to Install PostInit Patches)
+			auto MessagingInterface = F4SE::GetMessagingInterface();
+			MessagingInterface->RegisterListener(F4SEMessageListener);
+			REX::INFO("Started Listening for F4SE Message Callbacks.");
 
-		isInit = true;
+			// Install preload patches
+			moduleManager.QueryPreloadAll();
+			moduleManager.InstallPreloadAll();
+
+			isInit = true;
+		});
+
 		return isInit;
 	}
 }
