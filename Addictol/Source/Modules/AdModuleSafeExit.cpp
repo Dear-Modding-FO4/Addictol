f927ff3f6014f6eae802d0e9cbc44f0258e45a38
Alexey <vlad.shabalin.1992@mail.ru>
2026-02-10 14:06:44 +0300

    refactoring exit safe patch

--- Addictol/Source/Modules/AdModuleSafeExit.cpp
+++ Addictol/Source/Modules/AdModuleSafeExit.cpp
@@ -32,30 +32,8 @@ namespace Addictol
 
 	bool ModuleSafeExit::DoInstall([[maybe_unused]] F4SE::MessagingInterface::Message* a_msg) noexcept
 	{
-		REL::Relocation<std::uintptr_t> Target;
-		std::size_t Offset;
-
-		if (RELEX::IsRuntimeAE())
-		{
-			// AE
-			Target = REL::ID(4812562);
-			Offset = 0x20;
-		}
-		else if (RELEX::IsRuntimeNG())
-		{
-			// NG
-			Target = REL::ID(2718225);
-			Offset = 0x20B;
-		}
-		else
-		{
-			// OG
-			Target = REL::ID(668528);
-			Offset = 0x20;
-		}
-
 		auto& trampoline = REL::GetTrampoline();
-		trampoline.write_call<5>(Target.address() + Offset, Shutdown);
+		trampoline.write_call<5>(REL::Relocation{ REL::ID{ 668528, 2718225, 4812562 }, REL::Offset{ 0x20, 0x20B, 0x20 } }.get(), Shutdown);
 
 		return true;
 	}
