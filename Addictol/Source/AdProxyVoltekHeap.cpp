6f7bac7d71c848d6708ab0742b3a113348e0e09c
Alexey <vlad.shabalin.1992@mail.ru>
2026-02-14 15:39:56 +0300

    proactive protection against CTD when load latest save after dead player

--- Addictol/Source/AdProxyVoltekHeap.cpp
+++ Addictol/Source/AdProxyVoltekHeap.cpp
@@ -38,7 +38,20 @@ namespace Addictol
 
 	void ProxyVoltekHeap::free(void* lpBlock) const noexcept
 	{
-		voltek::scalable_free(lpBlock);
+		__try
+		{
+			voltek::scalable_free(lpBlock);
+		}
+		__except (1)
+		{
+			// CTD: free memory no vmm maybe
+			// malloc excluded - this hooked
+
+			// [2] 0x7FF6BD6600C1     Fallout4.exe+01E00C1	nop |  sub_1401E0080_1E00C1	nop
+			// [3] 0x7FF6BD796BD1     Fallout4.exe+0316BD1	mov rsi, [rsp + 0x38] | sub_140316B80_316BD1	mov rsi, [rsp + 0x38]
+
+			// this called MemoryManager::Deallocate (maybe bug game???)
+		}
 	}
 
 	void ProxyVoltekHeap::aligned_free(void* lpBlock) const noexcept
