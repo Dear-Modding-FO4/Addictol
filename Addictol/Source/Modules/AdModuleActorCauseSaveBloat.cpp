844d20339d0494cc168f7d9fb17740d50d19af95
Kuz <kuzeygokk@outlook.com>
2026-06-20 23:23:47 -0700

    incorrect init lock

--- Addictol/Source/Modules/AdModuleActorCauseSaveBloat.cpp
+++ Addictol/Source/Modules/AdModuleActorCauseSaveBloat.cpp
@@ -21,7 +21,7 @@ namespace Addictol
 			if (!a_cell)
 				return references_vector;
 
-			const auto lock = RE::BSSpinLock{ a_cell->spinLock };
+			const RE::BSSpinLock lock{ a_cell->spinLock };
 
 			for (auto& reference_pointer : a_cell->references)
 			{
