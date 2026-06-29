91597701a55ac038edc88ad5127ccc275b6a8de3
Alexey <vlad.shabalin.1992@mail.ru>
2026-06-30 00:34:22 +0300

    Remade module ReferenceHandleLimit;

--- Addictol/Source/Modules/AdModuleReferenceHandleLimitWarning.cpp
+++ Addictol/Source/Modules/AdModuleReferenceHandleLimitWarning.cpp
@@ -5,6 +5,8 @@
 #include <RE/C/ConsoleLog.h>
 #include <RE/B/BSSpinLock.h>
 #include <RE/B/BSPointerHandle.h>
+#include <RE/T/TESObjectREFR.h>
+#include <RE/B/BGSSaveLoadManager.h>
 
 namespace Addictol
 {
@@ -13,6 +15,7 @@ namespace Addictol
 	constexpr auto WARNING_MIN_RATIO = 0.90f;
 
 	static REX::TOML::Bool<> bWarningsReferenceHandleLimit{ "Warnings"sv, "bReferenceHandleLimit"sv, true };
+	static std::atomic_bool againReport_OutOfEntries_ReferenceHandleLimit{ false };
 
 	class HandleManager :
 		public RE::BSPointerHandleManagerInterface<RE::TESObjectREFR>
@@ -33,59 +36,20 @@ namespace Addictol
 		inline static constexpr uint32_t MAX_HANDLE_LIMIT = 1 << 21;
 
 		static uint32_t GetCount() noexcept;
-		static RE::BSPointerHandle<RE::TESObjectREFR> HkCreateHandle(RE::TESObjectREFR* a_ptr) noexcept;
+
 		static void DebugInfo(const std::string_view& a_eventName) noexcept;
 
 		inline static std::atomic_bool exceededShow{ false };
 		inline static HandleManager* singleton{ nullptr };
-		inline static decltype(HkCreateHandle)* CreateHandle_orig{ nullptr };
+
+		static void OutOfEntriesMessage() noexcept;
 	};
 
 	uint32_t HandleManager::GetCount() noexcept
 	{
 		auto manager = HandleManager::singleton;
 		if (!manager || (manager->freeListHead == manager->freeListTail == (MAX_HANDLE_LIMIT - 1))) return 0;
-		return manager ? manager->freeListHead : 0;
-	}
-
-	RE::BSPointerHandle<RE::TESObjectREFR> HandleManager::HkCreateHandle(RE::TESObjectREFR* a_ptr) noexcept
-	{
-		uint32_t count = HandleManager::GetCount();
-		double ratio = static_cast<double>(count) / static_cast<float>(MAX_HANDLE_LIMIT);
-		if (ratio >= WARNING_MIN_RATIO)
-		{
-			if (ratio >= 0.999f)
-			{
-				REX::CRITICAL("OUT OF HANDLE ARRAY ENTRIES! TERMINATE GAME FORCIBLY!"sv);
-				REX::W32::TerminateProcess(REX::W32::GetCurrentProcess(), ERANGE);
-			}
-			else if (!HandleManager::exceededShow)
-			{
-				HandleManager::exceededShow = true;
-				ratio *= 100.f;
-
-				REX::WARN("HANDLE ARRAY ENTRIES ALMOST EXCEEDED {:.1f}%"sv, ratio);
-
-				char szBuf[REX::W32::MAX_PATH]{};
-				sprintf_s(szBuf, "Addictol::ReferenceHandleLimitWarning: HANDLE ARRAY ENTRIES ALMOST EXCEEDED (%.1f%%)", ratio);
-
-				auto* consoleLog = RE::ConsoleLog::GetSingleton();
-				if (consoleLog)
-					consoleLog->AddString(szBuf);
-
-				std::thread th([]{
-					// 5 minutes in milliseconds
-					std::chrono::milliseconds delay(5 * 60 * 1000);
-					std::this_thread::sleep_for(delay);
-					// reset
-					HandleManager::exceededShow = false;
-					});
-				th.detach();
-			}
-		}
-
-		assert(CreateHandle_orig);
-		return CreateHandle_orig(a_ptr);
+		return manager && (manager->freeListTail != (MAX_HANDLE_LIMIT - 1)) ? manager->freeListTail : 0;
 	}
 
 	void HandleManager::DebugInfo(const std::string_view& a_eventName) noexcept
@@ -94,6 +58,19 @@ namespace Addictol
 		double ratio = static_cast<double>(count) / static_cast<float>(MAX_HANDLE_LIMIT);
 
 		REX::INFO("Reference Handle Count ({}): {}. Ratio: {:.1f}%"sv, a_eventName, count, ratio * 100.f);
+	}
+
+	void HandleManager::OutOfEntriesMessage() noexcept
+	{
+		// Unfortunately, the counter is never reset, only starting the game again.
+		// Saves game and quit
+
+		auto manager = RE::BGSSaveLoadManager::GetSingleton();
+		if (manager && !againReport_OutOfEntries_ReferenceHandleLimit.load())
+		{
+			REX::ERROR("OUT OF HANDLE ARRAY ENTRIES! SAVES GAME AND QUIT FORCIBLY!");
+			manager->QueueSaveLoadTask(RE::BGSSaveLoadManager::QUEUED_TASK::kSaveAndQuitToDesktop);
+		}
 	}
 
 	ModuleReferenceHandleLimitWarning::ModuleReferenceHandleLimitWarning() :
@@ -110,11 +87,26 @@ namespace Addictol
 	{
 		if (!a_msg)
 		{
-			HandleManager::singleton = reinterpret_cast<HandleManager*>(REL::VariantID(665313, 2688741, 4796005).address());
+			// Restore "OUT OF HANDLE ARRAY ENTRIES.  Null handle created for pointer %p." from CK.
 
-			*(uintptr_t*)&HandleManager::CreateHandle_orig = RELEX::DetourJump(
-				RE::ID::BSPointerHandle::BSPointerHandleManagerInterface::CreateHandle.address(),
-				(uintptr_t)&HandleManager::HkCreateHandle);
+			// Refr
+			RELEX::DetourCall(REL::Relocation(RE::ID::BSPointerHandle::BSPointerHandleManagerInterface::CreateHandle,
+				REL::Offset{ 0xA5 }).address(), (uintptr_t)&HandleManager::OutOfEntriesMessage);
+			// Actor
+			RELEX::DetourCall(REL::Relocation(REL::VariantID{ 1317100, 2188675 }, REL::Offset{ 0xA5, 0xC6 }).address(),
+				(uintptr_t)&HandleManager::OutOfEntriesMessage);
+			// Hazard
+			RELEX::DetourCall(REL::Relocation(REL::VariantID{ 472884, 2226228 }, REL::Offset{ 0xA5, 0xC6 }).address(),
+				(uintptr_t)&HandleManager::OutOfEntriesMessage);
+			// Projectile
+			RELEX::DetourCall(REL::Relocation(REL::VariantID{ 505174, 2228633 }, REL::Offset{ 0xA5, 0xC6 }).address(),
+				(uintptr_t)&HandleManager::OutOfEntriesMessage);
+			// Explosion
+			RELEX::DetourCall(REL::Relocation(REL::VariantID{ 1478967, 2236714 }, REL::Offset{ 0xA5, 0xC6 }).address(),
+				(uintptr_t)&HandleManager::OutOfEntriesMessage);
+
+			// Get actual manager
+			HandleManager::singleton = reinterpret_cast<HandleManager*>(REL::VariantID(665313, 2688741, 4796005).address());
 
 			return true;
 		}
