22482186d06fe6e568005f7144427fb5d5833612
TheGamerX20 <59514885+TheGamerX20@users.noreply.github.com>
2026-07-22 16:16:29 +0300

    Add Safety Checks to the Music Overlap Module

--- Addictol/Source/Modules/AdModuleMusicOverlap.cpp
+++ Addictol/Source/Modules/AdModuleMusicOverlap.cpp
@@ -13,6 +13,9 @@ namespace Addictol
 	{
 		static void DoFinish(RE::BSIMusicType* a_self, bool a_immediate)
 		{
+			if (!a_self || !a_self->tracks.data() || a_self->currentTrackIndex >= a_self->tracks.size() || !a_self->tracks[a_self->currentTrackIndex])
+				return;
+			
 			a_self->tracks[a_self->currentTrackIndex]->DoFinish(a_immediate, std::max(a_self->fadeTime, 4.0f));
 			a_self->typeStatus = a_self->tracks[a_self->currentTrackIndex]->GetMusicStatus();
 		}
