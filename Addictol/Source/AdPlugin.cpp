04cffe7382b6786696aa82fb24cb815599e7b869
TheGamerX20 <59514885+TheGamerX20@users.noreply.github.com>
2026-07-29 17:05:02 +0300

    Rework the Compatibility Checks to show a Message Box

--- Addictol/Source/AdPlugin.cpp
+++ Addictol/Source/AdPlugin.cpp
@@ -5,16 +5,17 @@
 #include <AdProfilerDLL.h>
 #include <AdProfilerMemory.h>
 #include <AdProfilerModules.h>
-#include <RE/B/BSScriptUtil.h>
-#include <RE/T/TESFile.h>
-#include <RE/T/TESDataHandler.h>
+
 #include <RE/B/BSCRC32.h>
+#include <RE/B/BSScriptUtil.h>
+#include <RE/T/TESDataHandler.h>
+#include <RE/T/TESFile.h>
+
+#include <Windows.h>
+#undef ERROR
 
 #define AD_LOGPLUGINHASHES 0
 //#define AD_DEBUGBREAK 1
-#if AD_DEBUGBREAK
-#	include <windows.h>
-#endif
 
 extern void AdRegisterModules();
 extern void AdRegisterPreloadModules();
@@ -46,68 +47,68 @@ namespace Addictol
 		return RE::detail::GenerateCRC32({ reinterpret_cast<uint8_t*>(strlwr(str.data())), str.length() });
 	}
 
-	static bool AnalyzeGameCollectionCriticalCompatibility() noexcept
+	static std::vector<const RE::TESFile*> AnalyzeGameCollectionCriticalCompatibility() noexcept
 	{
-		auto dataHandler = RE::TESDataHandler::GetSingleton();
-		if (!dataHandler) return false;
-
-		auto GetHash = [&](RE::TESFile* a_file)
+		// Incompatible Mods
+		std::vector<const RE::TESFile*> incompatibleMods;
+		const std::array<uint32_t, 6> incompatibleModHashes
 		{
-			std::string strFN = _strlwr(a_file->filename);
-			auto it = strFN.find_last_of('.');
-			if (it != std::string::npos) strFN.erase(it, -1);
-			return RE::detail::GenerateCRC32({ reinterpret_cast<uint8_t*>(strFN.data()), strFN.length() });
+			260600794, 1335048061,					// Fixer
+			2498600491, 2948692632, 631466042,		// Unbound Worldspace
+			2684648774								// Optimized Room Bounds
 		};
 
-		auto CheckHash = [&](uint32_t a_hash)
+		// Get DataHandler
+		auto dataHandler = RE::TESDataHandler::GetSingleton();
+		if (!dataHandler) return incompatibleMods;
+
+		// Is Compatible
+		auto IsCompatible = [&](const RE::TESFile* a_file)
 		{
-			std::array<uint32_t, 6> trash{ 
-				260600794, 1335048061,
-				2498600491, 2948692632, 631466042,
-				2684648774 };
-			return std::find(trash.begin(), trash.end(), a_hash) == trash.end();
+			// Get the Filename in Lowercase
+			std::string fileName = a_file->filename;
+			_strlwr(fileName.data());
+
+			// Remove the File Extension
+			auto it = fileName.find_last_of('.');
+			if (it != std::string::npos) fileName.erase(it, -1);
+
+			// Generate CRC32 Hash
+			const auto hash = RE::detail::GenerateCRC32({ reinterpret_cast<uint8_t*>(fileName.data()), fileName.length() });
+
+#if AD_LOGPLUGINHASHES
+			// Log
+			REX::INFO("Plugin: {}, Hash: {} / 0x{:08X}", a_file->filename, hash, hash);
+#endif
+
+			return std::find(incompatibleModHashes.begin(), incompatibleModHashes.end(), hash) == incompatibleModHashes.end();
+		};
+
+		// Analyze Files Array
+		auto AnalyzeFilesArray = [&](const RE::BSTArray<RE::TESFile*>& files)
+		{
+			for (const auto* file : files)
+			{
+				if (!IsCompatible(file))
+				{
+					REX::ERROR("Found an incompatible mod: {}"sv, file->filename);
+					incompatibleMods.push_back(file);
+				}
+			}
 		};
 
 #if AD_LOGPLUGINHASHES
 		REX::INFO("======== LOGGING PLUGIN HASHES ========");
 #endif
 
-		bool Compatibility = true;
-		for (auto& file : dataHandler->compiledFileCollection.files)
-		{
-			auto hash = GetHash(file);
-
-#if AD_LOGPLUGINHASHES
-			REX::INFO("Plugin: {}, Hash: {} / 0x{:08X}", file->filename, hash, hash);
-#endif
-
-			if (!CheckHash(hash))
-			{
-				REX::ERROR("Incompatible mod: {}"sv, file->filename);
-				Compatibility = false;
-			}
-		}
-
-		for (auto& file : dataHandler->compiledFileCollection.smallFiles)
-		{
-			auto hash = GetHash(file);
-
-#if AD_LOGPLUGINHASHES
-			REX::INFO("Plugin: {}, Hash: {} / 0x{:08X}", file->filename, hash, hash);
-#endif
-
-			if (!CheckHash(hash))
-			{
-				REX::ERROR("Incompatible mod: {}"sv, file->filename);
-				Compatibility = false;
-			}
-		}
+		AnalyzeFilesArray(dataHandler->compiledFileCollection.files);
+		AnalyzeFilesArray(dataHandler->compiledFileCollection.smallFiles);
 
 #if AD_LOGPLUGINHASHES
 		REX::INFO("======== END OF PLUGIN HASHES ========");
 #endif
 
-		return Compatibility;
+		return incompatibleMods;
 	}
 
 	static void F4SEMessageListener(F4SE::MessagingInterface::Message* a_msg) noexcept
@@ -128,14 +129,42 @@ namespace Addictol
 
 			if (a_msg->type == F4SE::MessagingInterface::kGameLoaded)
 			{
-				if (!AnalyzeGameCollectionCriticalCompatibility())
+				const auto incompatibleMods = AnalyzeGameCollectionCriticalCompatibility();
+				if (!incompatibleMods.empty())
 				{
-					REX::CRITICAL("Incompatible mods are installed."
-						"Disable addictol or reinstall game and starts new game without trash mods."
-						"Terminate game!"sv);
+					std::string incompatibilityMessage = "Incompatible mods are installed, please disable Addictol or remove the following incompatible mods:\n\n";
 
-					*((int*)0) = 0;
-					__assume(0);
+					// Add Incompatible Mods to the Message
+					for (const auto* file : incompatibleMods)
+					{
+						if (file)
+						{
+							incompatibilityMessage += "  - ";
+							incompatibilityMessage += file->filename;
+							incompatibilityMessage += '\n';
+						}
+					}
+
+					incompatibilityMessage += "\nCheck the mod page's description for more info, we cannot provide support if you choose to ignore this warning and you do so at your own risk.";
+
+					// Log
+					REX::CRITICAL("{}"sv, incompatibilityMessage);
+
+					// Message Box on a separate thread in order to not stall the rest of the mod
+					// This is in case the user decides to just Alt + Tab or something like that
+					std::thread([message = std::move(incompatibilityMessage)]
+					{
+						while (1)
+						{
+							const auto result = MessageBoxA(nullptr, message.c_str(), "Addictol - Incompatible Mods", MB_ABORTRETRYIGNORE | MB_ICONERROR | MB_SETFOREGROUND | MB_TOPMOST);
+							if (result == IDRETRY)
+								continue;
+							else if (result == IDABORT)
+								REX::W32::TerminateProcess(REX::W32::GetCurrentProcess(), EXIT_FAILURE);
+
+							break;
+						}
+					}).detach();
 				}
 
 				moduleManager.LogSummary();
@@ -218,7 +247,7 @@ namespace Addictol
 			if (PapyrusInterface->Register([](RE::BSScript::IVirtualMachine* vm) -> bool {
 				F4SEPapyrusListener(vm);
 				return true; }))
-				REX::INFO("Started Listening for Papyrus Callbacks."sv);	
+				REX::INFO("Started Listening for Papyrus Callbacks."sv);
 
 			// Query patches
 			moduleManager.QueryLoadAll();
