ad5eb193c59f6fec9bb9b6df35657ac20b23479a
frakkin64 <258024685+frakkin64@users.noreply.github.com>
2026-07-31 03:41:11 -0700

    remove patch for BSGameDataSystemUtility::IsDLCMasterOrCCFile

--- Addictol/Source/Modules/AdModuleInitTints.cpp
+++ Addictol/Source/Modules/AdModuleInitTints.cpp
@@ -34,9 +34,6 @@ namespace Addictol
 			//else
 			//	RELEX::WriteSafeNop(REL::ID(2192358).address() + 0x47, 0x3B);
 
-			// There are a lot of checks in this function, it was also used in the facegen verification function.
-			// Sets return always 1 or true.		
-			RELEX::WriteSafe(REL::ID(RELEX::IsRuntimeNG() ? 2320239 : 7113302).address(), { 0xB0, 0x01, 0xC3, 0x90, 0x90 });
 			// Skipping the entire section, can't delete the pointer, otherwise will go back to where started.
 			RELEX::WriteSafe(REL::ID(2207492).address() + 0xF3, { 0xE9, 0x7D, 0x00, 0x00, 0x00, 0x90 });
 		}
@@ -54,9 +51,6 @@ namespace Addictol
 			//RELEX::WriteSafeNop(REL::ID(779416).address() + 0x21, 0x19);		
 			//RELEX::WriteSafeNop(REL::ID(1444263).address() + 0x54, 0x19);
 
-			// There are a lot of checks in this function, it was also used in the facegen verification function.
-			// Sets return always 1 or true.		
-			RELEX::WriteSafe(REL::ID(1088335).address(), { 0xB0, 0x01, 0xC3, 0x90, 0x90 });
 			// Skipping the entire section, can't delete the pointer, otherwise will go back to where started.
 			RELEX::WriteSafe(REL::ID(1264833).address() + 0xF9, { 0xE9, 0x9C, 0x00, 0x00, 0x00, 0x90 });
 		}
