fc9cc377dc09dafa6107ecfb8956ce88f7dfe867
TheGamerX20 <59514885+TheGamerX20@users.noreply.github.com>
2026-09-08 11:49:39 +0300

    Add Mod Check for Process Icon

--- Addictol/Source/Modules/AdModuleProcessIcon.cpp
+++ Addictol/Source/Modules/AdModuleProcessIcon.cpp
@@ -16,6 +16,17 @@ namespace Addictol
 		Module("Process Icon")
 	{}
 
+	bool ModuleProcessIcon::DoQuery() const noexcept
+	{
+		if (IsModDLLPresent("Fallout4ProcessIconFix.dll"))
+		{
+			Skip("standalone 'Fallout4ProcessIconFix.dll' is installed"sv);
+			return false;
+		}
+
+		return true;
+	}
+
 	bool ModuleProcessIcon::DoInstall([[maybe_unused]] F4SE::MessagingInterface::Message* a_msg) noexcept
 	{
 		RELEX::DetourCall(REL::Relocation(REL::ID{ 193854, 2276814 }, REL::Offset{ 0x92, 0x194 }).address(),
