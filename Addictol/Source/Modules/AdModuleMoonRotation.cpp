79b64f4eb62cec8a1c4ac17ed5c06103859772f5
Alexey <vlad.shabalin.1992@mail.ru>
2026-09-16 07:25:36 +0300

    feat: final moon rotation patch, removed init hook, I'm satisfied with the orbit

--- Addictol/Source/Modules/AdModuleMoonRotation.cpp
+++ Addictol/Source/Modules/AdModuleMoonRotation.cpp
@@ -15,56 +15,92 @@
 
 namespace Addictol
 {
-	constexpr float NI_PI = static_cast<float>(3.1415926535897932);
-	constexpr float NI_HALF_PI = 0.5f * NI_PI;
-
-	namespace Math
-	{
-		static void SetEulerAnglesXYZ(RE::NiMatrix3* a_matrix, float a_x, float a_y, float a_z) noexcept
-		{
-			const float sinX = std::sinf(a_x);
-			const float sinY = std::sinf(a_y);
-			const float sinZ = std::sinf(a_z);
-
-			const float cosX = std::cosf(a_x);
-			const float cosY = std::cosf(a_y);
-			const float cosZ = std::cosf(a_z);
-
-			a_matrix->entry[0][0] = cosY * cosZ;
-			a_matrix->entry[0][1] = cosY * sinZ;
-			a_matrix->entry[0][2] = -sinY;
-			a_matrix->entry[1][0] = sinX * sinY * cosZ - cosX * sinZ;
-			a_matrix->entry[1][1] = sinX * sinY * sinZ + cosX * cosZ;
-			a_matrix->entry[1][2] = sinX * cosY;
-			a_matrix->entry[2][0] = cosX * sinY * cosZ + sinX * sinZ;
-			a_matrix->entry[2][1] = cosX * sinY * sinZ - sinX * cosZ;
-			a_matrix->entry[2][2] = cosX * cosY;
-		}
-	}
-
 	namespace Moon
 	{
-		using TInitThunk = void(RE::Moon* a_moon, RE::NiNode* a_root);
-		static std::function<TInitThunk> Init_orig;
+		constexpr float NI_PI = static_cast<float>(3.1415926535897932);
 
-		static void Init(RE::Moon* a_moon, RE::NiNode* a_root) noexcept
+		// Function to convert degrees to radians
+		constexpr static double Deg2Rad(double a_degrees) noexcept
 		{
-			// appearance from the east
-			if (a_root) 
-				Math::SetEulerAnglesXYZ(std::addressof(a_root->local.rotate), .0f, .0f, NI_HALF_PI);
-			
-			//a_moon->size = 1000;
-
-			Init_orig(a_moon, a_root);
-
-			//a_moon->size = 1000;
+			return a_degrees * (NI_PI / 180.0);
 		}
 
-		static void HookUpdatePosition(RE::NiNode* a_root) noexcept
+		class LunarRotationCalculator
 		{
-			// movement from east to west (original: 19.75 to 7.0)
-			auto hour = reinterpret_cast<float*>((uintptr_t)a_root + 0x358);
-			*hour = 24.0f - *hour;
+			// The Moon rotates exactly 360 degrees in one Sidereal Month (27.321661 days)
+			const double DAYS_PER_SIDEREAL_MONTH = 27.321661;
+			const double HOURS_PER_DAY = 24.0;
+
+			// Derived total hours for a single full 360-degree rotation
+			const double HOURS_PER_ROTATION = DAYS_PER_SIDEREAL_MONTH * HOURS_PER_DAY;
+
+			// Degrees the moon rotates in exactly 1 hour (~0.549°/hr)
+			const double DEGREES_PER_HOUR = 360.0 / HOURS_PER_ROTATION;
+		public:
+			LunarRotationCalculator() = default;
+
+			double CalculateRotationDeg(double a_hours, bool a_clampTo360 = true) const noexcept
+			{
+				double totalDegrees = a_hours * DEGREES_PER_HOUR;
+
+				if (a_clampTo360)
+				{
+					totalDegrees = std::fmod(totalDegrees, 360.0);
+					if (totalDegrees < 0.0)
+						totalDegrees += 360.0; // Handle negative elapsed hours gracefully
+				}
+
+				return totalDegrees;
+			}
+
+			double CalculateRotationRad(double a_hours, bool a_clampTo360 = true) const noexcept
+			{
+				return Deg2Rad(CalculateRotationDeg(a_hours, a_clampTo360));
+			}
+		};
+
+		class Lunar
+		{
+			float angle{ 0.f };
+			float radius{ 600.f };
+			LunarRotationCalculator calc{};
+		public:
+			Lunar() = default;
+
+			void Update(RE::Moon* a_moon, RE::Sky* a_sky) noexcept
+			{
+				// We get the number of hours passed since the beginning of the game.
+				float hoursPassed = 0.f;
+				auto calendar = RE::Calendar::GetSingleton();
+				if (calendar) hoursPassed = calendar->GetHoursPassed();
+				else hoursPassed = a_sky->currentGameHour;
+
+				// Getting the actual angle of the moon by the hour, clipped by degrees, in radians.
+				angle = static_cast<float>(calc.CalculateRotationRad(hoursPassed));
+				auto& root = a_moon->root;
+
+				auto x = std::cos(angle) * radius;
+				auto z = std::sin(angle) * radius;
+
+				// The moon will be bigger at sunrise and sunset.
+				auto delta_size = std::fabs(z) - 325.f;
+
+				// We rotate the moon on the y-axis.
+				// Note: z-axis rotates the moon around the center.
+				root->local.translate.x = x;
+				root->local.translate.y = radius + delta_size;
+				root->local.translate.z = z;
+			}
+		};
+
+		static Lunar moon;
+		using TUpdateThunk = void(RE::Moon* a_moon, RE::Sky* a_sky, float a_unk);
+		static std::function<TUpdateThunk> Update_orig;
+
+		static void HookUpdateDirection(RE::Moon* a_moon, RE::Sky* a_sky, float a_unk) noexcept
+		{
+			Update_orig(a_moon, a_sky, a_unk);
+			moon.Update(a_moon, a_sky);
 		}
 	}
 
@@ -97,89 +133,15 @@ namespace Addictol
 
 	bool ModuleMoonRotation::DoInstall([[maybe_unused]] F4SE::MessagingInterface::Message* a_msg) noexcept
 	{
-		// Fixed starts
+		const auto targetInit = REL::ID{ 114988, 2208804 };
+		const auto targetUpdate = REL::ID{ 4410, 2208806 };
 
-		const auto target1 = REL::ID{ 114988, 2208804 };
-		Moon::Init_orig = reinterpret_cast<Moon::TInitThunk*>(RELEX::TryDetourJump(target1.address(),
-			reinterpret_cast<uintptr_t>(&Moon::Init), { 0x48, 0x89, 0x5C, 0x24, 0x10 }));
-		if (!Moon::Init_orig)
-		{
-			REX::WARN("Moon Rotation: unexpected bytes at target - skipping to avoid corruption."sv);
-			return false;
-		}
+		// Fixed rotation and direction
 
-		// Fixed direction
+		Moon::Update_orig = (Moon::TUpdateThunk*)RELEX::DetourJump(targetUpdate.address(),
+			reinterpret_cast<uintptr_t>(&Moon::HookUpdateDirection));
 
-		struct HookUpdatePositionPatch : Xbyak::CodeGenerator
-		{
-			HookUpdatePositionPatch(uintptr_t targetAddr, uintptr_t funcAddr)
-			{
-				// move node ptr
-				mov(rcx, rdi);
-
-				// call our function
-				sub(rsp, 0x20);	
-				mov(rax, funcAddr);
-				call(rax);
-				add(rsp, 0x20);
-
-				// orig code
-				movss(xmm6, dword[rdi + 0x358]);
-
-				// return back (ret)
-				jmp(ptr[rip]);
-				dq(targetAddr + 5);
-			}
-		};
-
-		struct HookUpdatePositionPatch_OG : Xbyak::CodeGenerator
-		{
-			HookUpdatePositionPatch_OG(uintptr_t targetAddr, uintptr_t funcAddr)
-			{
-				// move node ptr
-				mov(rcx, rdi);
-
-				// call our function
-				sub(rsp, 0x20);
-				mov(rax, funcAddr);
-				call(rax);
-				add(rsp, 0x20);
-
-				// orig code
-				movss(xmm3, dword[rdi + 0x358]);
-
-				// return back (ret)
-				jmp(ptr[rip]);
-				dq(targetAddr + 5);
-			}
-		};
-
-		if (RELEX::IsRuntimeOG())
-		{
-			const auto target2 = REL::ID(4410).address() + 0x7E;
-			if (RELEX::Validate(target2, { 0xF3, 0x0F, 0x10, 0x9F, 0x58, 0x03, 0x00, 0x00 }))
-				RELEX::XbyakJump<HookUpdatePositionPatch_OG>(target2, target2,
-					reinterpret_cast<uintptr_t>(&Moon::HookUpdatePosition));
-			else
-			{
-				REX::WARN("Moon Rotation: unexpected bytes at target - skipping to avoid corruption."sv);
-				return false;
-			}
-		}
-		else
-		{
-			const auto target2 = REL::ID(2208806).address() + 0x8A;
-			if (RELEX::Validate(target2, { 0xF3, 0x0F, 0x10, 0xB7, 0x58, 0x03, 0x00, 0x00 }))
-				RELEX::XbyakJump<HookUpdatePositionPatch>(target2, target2,
-					reinterpret_cast<uintptr_t>(&Moon::HookUpdatePosition));
-			else
-			{
-				REX::WARN("Moon Rotation: unexpected bytes at target - skipping to avoid corruption."sv);
-				return false;
-			}
-		}
-
-		const auto target3 = REL::Relocation{ target1, REL::Offset{ 0x1E2, 0x1F7 } }.address();
+		const auto target3 = REL::Relocation{ targetInit, REL::Offset{ 0x1E2, 0x1F7 } }.address();
 		if (!RELEX::Validate(target3, { 0x04, 0x48, 0x8B, 0x4E, 0x08 }))
 		{
 			REX::WARN("Moon Rotation: unexpected bytes at target - skipping to avoid corruption."sv);
