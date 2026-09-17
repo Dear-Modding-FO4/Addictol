bf5e31dc858b741758cd2ca55e908468781be56b
Alexey <vlad.shabalin.1992@mail.ru>
2026-09-17 21:29:27 +0300

    feat: now orbit the moon is tilted at approximately 5.14° and move it to the south Boston, since Boston is north of the equator

--- Addictol/Source/Modules/AdModuleMoonRotation.cpp
+++ Addictol/Source/Modules/AdModuleMoonRotation.cpp
@@ -28,7 +28,12 @@ namespace Addictol
 		class LunarRotationCalculator
 		{
 			// The Moon rotates exactly 360 degrees in one Sidereal Month (27.321661 days)
+#if 0
+			// FOR TEST
+			const double DAYS_PER_SIDEREAL_MONTH = .00321661;
+#else
 			const double DAYS_PER_SIDEREAL_MONTH = 27.321661;
+#endif
 			const double HOURS_PER_DAY = 24.0;
 
 			// Derived total hours for a single full 360-degree rotation
@@ -36,8 +41,14 @@ namespace Addictol
 
 			// Degrees the moon rotates in exactly 1 hour (~0.549°/hr)
 			const double DEGREES_PER_HOUR = 360.0 / HOURS_PER_ROTATION;
+
+			// The Moon's orbit is tilted at approximately 5.14° relative to Earth's orbital plane (the ecliptic)
+			const float DEGREES_INCLINATION = 5.14f;
+
+			const float cosInclinationTheta{ std::cosf(static_cast<float>(Deg2Rad(DEGREES_INCLINATION))) };
+			const float sinInclinationTheta{ std::sinf(static_cast<float>(Deg2Rad(DEGREES_INCLINATION))) };
 		public:
-			LunarRotationCalculator() = default;
+			constexpr LunarRotationCalculator() = default;
 
 			double CalculateRotationDeg(double a_hours, bool a_clampTo360 = true) const noexcept
 			{
@@ -57,12 +68,19 @@ namespace Addictol
 			{
 				return Deg2Rad(CalculateRotationDeg(a_hours, a_clampTo360));
 			}
+
+			void ApplyInclination(RE::NiPoint3& a_translate) const noexcept
+			{
+				a_translate.y = a_translate.y * cosInclinationTheta - a_translate.x * sinInclinationTheta;
+				a_translate.x = a_translate.y * sinInclinationTheta + a_translate.x * cosInclinationTheta;
+			}
 		};
 
 		class Lunar
 		{
 			float angle{ 0.f };
 			float radius{ 600.f };
+			RE::NiPoint3 coordinate{};
 			LunarRotationCalculator calc{};
 		public:
 			Lunar() = default;
@@ -70,7 +88,7 @@ namespace Addictol
 			void Update(RE::Moon* a_moon, RE::Sky* a_sky) noexcept
 			{
 				// We get the number of hours passed since the beginning of the game.
-				float hoursPassed = 0.f;
+				auto hoursPassed = 0.f;
 				auto calendar = RE::Calendar::GetSingleton();
 				if (calendar) hoursPassed = calendar->GetHoursPassed();
 				else hoursPassed = a_sky->currentGameHour;
@@ -79,17 +97,25 @@ namespace Addictol
 				angle = static_cast<float>(calc.CalculateRotationRad(hoursPassed));
 				auto& root = a_moon->root;
 
-				auto x = std::cos(angle) * radius;
-				auto z = std::sin(angle) * radius;
+				// Calculate coordinate
+				root->local.translate.x = std::cosf(angle) * radius;
+				root->local.translate.z = std::sinf(angle) * radius;
+				// Let's move it to the south, since Boston is north of the equator.
+				root->local.translate.y = -325.f;
+
+				// Apply inclination tilted at approximately 5.14° relative to Earth's orbital plane.
+				calc.ApplyInclination(root->local.translate);
 
 				// The moon will be bigger at moonrise and moonset.
-				auto delta_size = std::fabs(z) - 325.f;
-
-				// We rotate the moon on the y-axis.
-				// Note: z-axis rotates the moon around the center.
-				root->local.translate.x = x;
-				root->local.translate.y = radius + delta_size;
-				root->local.translate.z = z;
+				root->local.scale = std::fmaxf(1.0f - std::fabs(root->local.translate.z) / radius, 0.5f);
+#if 0
+				// FOR TEST
+				root->local.translate.x += 1200.f;
+				root->local.translate.z += 1200.f;
+#else
+				// Place it a little lower so that you don't see how the moon turns at the end of the map.
+				root->local.translate.z -= 120.f;
+#endif
 			}
 		};
 
